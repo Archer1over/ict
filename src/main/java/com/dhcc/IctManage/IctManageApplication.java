3060f467ee992319b483d01e33769929e96c528f
CuiYang16 <cuiyangmail@163.com>
2018-12-11 09:20:31 +0800

    f

---
+++ src/main/java/com/dhcc/IctManage/IctManageApplication.java
@@ -0,0 +1,12 @@
+package com.dhcc.IctManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IctManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IctManageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dhcc/IctManage/IctManageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dhcc.IctManage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IctManageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
